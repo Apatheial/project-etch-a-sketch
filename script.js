d5d10df209b986a35bfd96145679193cf7465c5e
¨Gabriel¨ <129301298+Apatheial@users.noreply.github.com>
2025-03-27 18:45:19 -0300

    feat: complete the script for the Sketch Pad
    Added a function to calculate the number of
    squares for the sketch pad. Added a function to
    remove the previous sketch pad and a function to
    create a new one.
    
    Added a button with a prompt to get the number of
    squares for the new sketch pad and create it.

--- script.js
+++ script.js
@@ -1,11 +1,57 @@
 const main = document.querySelector("#main");
 
-for (let i = 0; i < 256; i++) {
-    const squareDiv = document.createElement("div");
-    squareDiv.setAttribute("class", "square-div");
-    squareDiv.addEventListener("mouseover", () => {
-        squareDiv.classList.toggle("mouseover-div");
-    })
+const calculateSquares = (squaresNumber) => {
+    const MAX_WIDTH = 800;
+    let totalSquare;
+    let squareSize;
 
-    main.appendChild(squareDiv)
+    if (squaresNumber < 4) {
+        totalSquare = 16 * 16;
+        squareSize = MAX_WIDTH / 16;
+        return {totalSquare, squareSize};
+    }
+
+    if (squaresNumber > 100) {
+        totalSquare = 100 * 100;
+        squareSize = MAX_WIDTH / 100;
+        return {totalSquare, squareSize};
+    }
+
+    totalSquare = squaresNumber * squaresNumber;
+    squareSize = MAX_WIDTH / squaresNumber;
+    return {totalSquare, squareSize};
 }
+
+const createSketchPad = (squaresNumber = 16) => {
+    const {totalSquare, squareSize} = calculateSquares(squaresNumber);
+    removeSketchPad();
+    
+    for (let i = 0; i < totalSquare; i++) {
+        const squareDiv = document.createElement("div");
+        squareDiv.setAttribute("class", "square-div");
+        squareDiv.style.width = `${squareSize}px`;
+        squareDiv.style.height = `${squareSize}px`;
+        main.appendChild(squareDiv)
+    }
+}
+
+const removeSketchPad = () => { 
+    while (main.firstChild) {
+        main.removeChild(main.lastChild);
+    }
+}
+
+main.addEventListener("mouseover", (element) => {
+    if (element.target.classList.contains("square-div")) {
+        element.target.classList.add("mouseover-div");
+    }
+})
+
+const button = document.querySelector("#btn-sketch-pad");
+
+button.addEventListener("click", () => {
+    const squareNumber = parseInt(prompt("Enter the number of square per side (MAX = 100):"));
+    isNaN(squareNumber) ? createSketchPad() : createSketchPad(squareNumber);
+})
+
+createSketchPad();
